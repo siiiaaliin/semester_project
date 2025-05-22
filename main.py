1fa8e512804be67aba1dce5001722d6a6d6004e8
Angelina-sin <angelinaryaboy2007@gmail.com>
2025-05-22 13:29:55 +0300

    Added main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *  
+
+root = Tk()
+root.title("Memory Game")  
+
+frame = Frame(root)  
+frame.pack()  
+
+ROWS = 5
+COLS = 5 #поле задаю параметр
+
+
+buttons = []
+
+for row in range(ROWS):
+    row_buttons = []  
+    for col in range(COLS):
+        
+        btn = Button(frame,
+                     text="",      #поки порожній          
+                     width=18, height=9,      
+                     command=lambda r=row, c=col: on_button_click(r, c))  
+        btn.grid(row=row, column=col)  
+        row_buttons.append(btn)  
+    buttons.append(row_buttons)
+
+def on_button_click(row, col):
+    
+    btn = buttons[row][col]  
+    btn["text"] = "поки без картинки :("        # імітація відкриття картинки
+
+root.mainloop()
