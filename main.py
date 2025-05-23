8b3e57164816fbdd86970265b59955f7348cd7d5
Angelina-sin <angelinaryaboy2007@gmail.com>
2025-05-23 14:00:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from tkinter import *
 
 root = Tk()
 root.title("Memory Game")  
+root.configure(bg="lightpink")
 
-frame = Frame(root)  
+label = Label(root, text="Memory Game", 
+              font=("Times New Romans", 16), bg="lightpink")
+label.pack(pady=10)
+
+frame = Frame(root, bg="lightblue")  
 frame.pack()  
 
 ROWS = 4
 COLS = 4 #поле задаю параметр
 
+game=GameData(rows=ROWS, cols=COLS) 
+back_img=PhotoImage(file="/Users/alinastratiichuk/Desktop/back.png") 
 
 buttons = []
 
@@ -18,15 +25,20 @@ for row in range(ROWS):
         
         btn = Button(frame,
                      text="",      #поки порожній          
-                     width=18, height=9,      
+                     width=100, height=100,      
+                     image=back_img,
                      command=lambda r=row, c=col: on_button_click(r, c))  
-        btn.grid(row=row, column=col)  
+        btn.grid(row=row, column=col,padx=2, pady=2)  
         row_buttons.append(btn)  
     buttons.append(row_buttons)
 
 def on_button_click(row, col):
     
     btn = buttons[row][col]  
-    btn["text"] = "поки без картинки :("        # імітація відкриття картинки
+    btn["image"] = game.get_image(row, col)       
+
+exit_button = Button(root, text="Вийти", font=("Times New Romans", 16), 
+                     command=root.destroy)
+exit_button.pack(pady=15)
 
 root.mainloop()
